1a72079160100601e5ac5db0b53f77aad6dd8b83
hadit10 <haditya.prasetyo.hp@gmail.com>
2023-11-08 15:19:09 +0700

    menambahkan beberapa folder dan file

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,34 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+<style>
+table, tr, td{
+        text-align: center;
+    }
+table{
+    width: 100%;
+}
+</style>
 <body>
-    Hello World!
+    <form>
+        <table align="center" bgcolor="lightblue" cellspacing="1" cellpadding="5" border="3">
+            <tr><img src="oke.jpg"></tr>
+            <tr>
+                <td rowspan="2">Nomor</td>
+                <td rowspan="2">Nama</td>
+                <td colspan="2">Pangkat</td>
+                <td colspan="2">Jabatan</td>
+                <td colspan="2">Masa Kerja</td>
+            </tr>
+            <tr>
+                <td>Gol/Ruang</td>
+                <td>TMT</td>
+                <td>Nama</td>
+                <td>TMT</td>
+                <td>Tahun</td>
+                <td>Bulan</td>
+            </tr>
+        </table>
+    </form>
 </body>
 </html>
